2c0479489e51e64a55c81fac3520f929f2a626d0
Saransh Mehra <saransh.mehra@nagarro.com>
2019-11-04 13:48:23 +0530

    refactoring

--- src/container/Campaign/Campaign.test.js
+++ src/container/Campaign/Campaign.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Campaign from './Campaign';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Campaign />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<Campaign />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
